1b03711e981e8afd1f392d50217225d6c2302cdb
alexander.caesario <alexandercaesariobangun@gmail.com>
2022-09-27 00:15:08 +0700

    [REFACTOR] refactor password validation test so password is directly supplied as argument

--- utils/validators/passwordValidator/passwordValidator.test.ts
+++ utils/validators/passwordValidator/passwordValidator.test.ts
@@ -2,40 +2,35 @@ import { passwordValidator } from "./passwordValidator";
 
 describe("Password validator test", () => {
   it("Invalid Password: Less than 8 characters", () => {
-    const password = "abdc";
-    const [isValid, error] = passwordValidator(password);
+    const [isValid, error] = passwordValidator("abdc");
 
     expect(isValid).toBe(false);
     expect(error).toBe("Your password should have at least 8 characters");
   });
 
   it("Invalid Password: No numbers", () => {
-    const password = "abdcdeasaADf";
-    const [isValid, error] = passwordValidator(password);
+    const [isValid, error] = passwordValidator("abdcdeasaADf");
 
     expect(isValid).toBe(false);
     expect(error).toBe("Your password should have at least 1 number");
   });
 
   it("Invalid Password: No lowercases", () => {
-    const password = "ABVSDFSAFSDC";
-    const [isValid, error] = passwordValidator(password);
+    const [isValid, error] = passwordValidator("ABVSDFSAFSDC");
 
     expect(isValid).toBe(false);
     expect(error).toBe("Your password should have at least 1 lowercase letter");
   });
 
   it("Invalid Password: No uppercases", () => {
-    const password = "fajsdklcsxajkl";
-    const [isValid, error] = passwordValidator(password);
+    const [isValid, error] = passwordValidator("fajsdklcsxajkl");
 
     expect(isValid).toBe(false);
     expect(error).toBe("Your password should have at least 1 uppercase letter");
   });
 
   it("Valid password", () => {
-    const password = "RashadAlekJoAryaIndi123";
-    const [isValid, error] = passwordValidator(password);
+    const [isValid, error] = passwordValidator("RashadAlekJoAryaIndi123");
 
     expect(isValid).toBe(true);
     expect(error).toBe("");
