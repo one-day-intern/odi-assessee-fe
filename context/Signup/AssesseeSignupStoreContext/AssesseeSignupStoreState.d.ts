6790d9242e6f913741fd30310ed0bcfb54eab1ac
alexander.caesario <alexandercaesariobangun@gmail.com>
2022-10-09 19:03:23 +0700

    [GREEN] implement assessee signup store and assessee signup step contexts, to store the data in the multistep form and as multistep toggle

---
+++ context/Signup/AssesseeSignupStoreContext/AssesseeSignupStoreState.d.ts
@@ -0,0 +1,17 @@
+interface AssesseeSignupStoreState {
+    data: AssesseeSignupStoreElements;
+    errors: AssesseeSignupStoreElements;
+    setValue: (name: string, value: string) => void;
+    setError: (name: string, value: string) => void;
+    postResult: () => void;
+}
+
+interface AssesseeSignupStoreElements {
+    email: string;
+    password: string;
+    confirmed_password: string;
+    first_name: string;
+    last_name: string;
+    date_of_birth: string;
+    phone_number: string;
+}
