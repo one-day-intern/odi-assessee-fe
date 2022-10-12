e20959af32ba6150ed0f5a329c159b29f9a19093
rashadaziz <rashad.aziz@ui.ac.id>
2022-10-12 17:57:23 +0700

    [REFACTOR] add link to go back to landing page
    We want to give the user the option to leave the preview room withouth joining the conference, thus we decided to add a back to main page link that will take the users back to the video conference landing page

--- components/features/Dashboard/Applications/VideoConference/Room/PreviewVideo/PreviewVideo.tsx
+++ components/features/Dashboard/Applications/VideoConference/Room/PreviewVideo/PreviewVideo.tsx
@@ -1,13 +1,10 @@
 import React from "react";
-import {
-  useHMSStore,
-  selectLocalPeer,
-} from "@100mslive/react-sdk";
+import { useHMSStore, selectLocalPeer } from "@100mslive/react-sdk";
 import styles from "./PreviewVideo.module.css";
 import VideoPlayer from "../../VideoPlayer";
 import Controls from "../../Controls";
 import { useDashboardAPI } from "@context/Dashboard/DashboardAPIContext";
-import { Button } from "@components/shared/elements/Button"
+import { Button } from "@components/shared/elements/Button";
 
 interface Props {
   children?: React.ReactNode;
@@ -15,7 +12,10 @@ interface Props {
   onLeaveConference: () => void;
 }
 
-const PreviewVideo: React.FC<Props> = ({ onJoinConference, onLeaveConference }) => {
+const PreviewVideo: React.FC<Props> = ({
+  onJoinConference,
+  onLeaveConference,
+}) => {
   const peer = useHMSStore(selectLocalPeer);
   const { app } = useDashboardAPI();
 
@@ -23,16 +23,29 @@ const PreviewVideo: React.FC<Props> = ({ onJoinConference, onLeaveConference }) 
     (app?.width >= 1280 || app.fullscreen) && window.innerWidth >= 1280;
 
   return (
-    <div className={`${styles["preview-video_container"]} ${normalMode ? styles.wide : ""}`}>
+    <div
+      className={`${styles["preview-video_container"]} ${
+        normalMode ? styles.wide : ""
+      }`}
+    >
       <div className={`${styles["preview-player_container"]}`}>
         <VideoPlayer peer={peer} />
       </div>
-      <div className={`${styles["preview-video_controls"]} ${normalMode ? "" : styles.mobile}`}>
-        <h1 className={`${styles["preview-video_header"]}`}>
-          Ready to join?
-        </h1>
+      <div
+        className={`${styles["preview-video_controls"]} ${
+          normalMode ? "" : styles.mobile
+        }`}
+      >
+        <h1 className={`${styles["preview-video_header"]}`}>Ready to join?</h1>
         <Controls />
-        <Button variant="secondary" onClick={() => onJoinConference()} style={{ maxWidth: 200, fontSize: "1em",fontWeight: "bold" }}>Join Conference</Button>
+        <Button
+          variant="secondary"
+          onClick={() => onJoinConference()}
+          style={{ maxWidth: 200, fontSize: "1em", fontWeight: "bold", padding: "0.75rem 1rem", marginBottom: 0 }}
+        >
+          Join Conference
+        </Button>
+        <a role="button" className={styles.back} onClick={() => onLeaveConference()}>Or, go back to the main page</a>
       </div>
     </div>
   );
