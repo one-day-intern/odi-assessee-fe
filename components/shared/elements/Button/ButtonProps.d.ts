35901db5ff6a129de34fd34d224ed3af393761e6
alexander.caesario <alexandercaesariobangun@gmail.com>
2022-10-12 16:31:16 +0700

    [REFACTOR] add styles props to buttons

--- components/shared/elements/Button/ButtonProps.d.ts
+++ components/shared/elements/Button/ButtonProps.d.ts
@@ -1,8 +1,9 @@
-import { ReactNode } from "react";
+import { ReactNode, CSSProperties } from "react";
 
 interface ButtonProps {
   children: ReactNode;
   variant: "primary" | "secondary";
   onClick?: () => void;
   disabled?: boolean;
+  style?: CSSProperties
 }
