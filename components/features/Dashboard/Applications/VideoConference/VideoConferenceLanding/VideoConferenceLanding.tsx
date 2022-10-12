19896b146cbf22a2bd34e97489daee508a4bc024
rashadaziz <rashad.aziz@ui.ac.id>
2022-10-12 16:46:18 +0700

    [SETUP] create stubs and interfaces for Video Conference Landing component

---
+++ components/features/Dashboard/Applications/VideoConference/VideoConferenceLanding/VideoConferenceLanding.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+interface Props {
+    onEnterConference: () => void;
+}
+
+const VideoConferenceLanding: React.FC<Props> = ({ onEnterConference }) => {
+  return (
+    <></>
+  )
+}
+
+export default VideoConferenceLanding
