ab5091c350d0c188d3b033e533d539badece9305
rashadaziz <rashad.aziz@ui.ac.id>
2022-10-13 10:43:32 +0700

    [REFACTOR] refactor Video Conference landing page styles
    Refactored the styling of the video conference landing page to be production ready. Buttons are also now using the shared element to ensure consistency. Artwork has also been added. The landing page is also fully responsive in that it reacts to resize events even in a virtual window component

--- components/features/Dashboard/Applications/VideoConference/VideoConferenceLanding/VideoConferenceLanding.tsx
+++ components/features/Dashboard/Applications/VideoConference/VideoConferenceLanding/VideoConferenceLanding.tsx
@@ -1,13 +1,65 @@
-import React from 'react'
+import React from "react";
+import styles from "./VideoConferenceLanding.module.css";
+import ConferenceArtwork from "./ConferenceArtwork";
+import { Button } from "@components/shared/elements/Button";
+import { OdiLogo } from "@components/shared/svg/OdiLogo";
+import { useDashboardAPI } from "@context/Dashboard/DashboardAPIContext";
 
 interface Props {
-    onEnterConference: () => void;
+  onEnterConference: () => void;
+  onEnterConferenceWithPreview: () => void;
 }
 
-const VideoConferenceLanding: React.FC<Props> = ({ onEnterConference }) => {
+const buttonStyles: React.CSSProperties = {
+  fontSize: "1em",
+  fontWeight: "bold",
+  maxWidth: 350,
+  margin: "1rem",
+};
+
+const VideoConferenceLanding: React.FC<Props> = ({
+  onEnterConference,
+  onEnterConferenceWithPreview,
+}) => {
+  const { app } = useDashboardAPI();
+
+  const normalMode =
+    (app?.width >= 1220 || app.fullscreen) && window.innerWidth >= 1220;
+
   return (
-    <button data-testid="EnterConference" onClick={() => onEnterConference()}>Enter Conference</button>
-  )
-}
+    <div
+      data-testid="EnterConference"
+      className={`${styles["landing-container"]}`}
+    >
+      {normalMode && (
+        <section className={`${styles.section}`}>
+          <ConferenceArtwork />
+        </section>
+      )}
+      <section className={`${styles.section}`}>
+        <div className={`${styles["svg-wrapper"]}`}>
+          <OdiLogo />
+        </div>
+        <h1 className={`${styles.title} ${normalMode ? "" : styles.mobile}`}>
+          Video Conferencing
+        </h1>
+        <Button
+          style={buttonStyles}
+          variant="secondary"
+          onClick={() => onEnterConferenceWithPreview()}
+        >
+          Preview Join
+        </Button>
+        <Button
+          style={buttonStyles}
+          variant="primary"
+          onClick={() => onEnterConference()}
+        >
+          Join Conference
+        </Button>
+      </section>
+    </div>
+  );
+};
 
-export default VideoConferenceLanding
+export default VideoConferenceLanding;
