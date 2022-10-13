a12dc13f343efb8fe3ae2218b17f1b8a238d3792
rashadaziz <rashad.aziz@ui.ac.id>
2022-10-13 13:42:35 +0700

    [REFACTOR] refactor styling of settings modal and now support device selection
    The user is now able to change their devices based on their preferences, the settings modal is also now styled and ready for production

--- components/features/Dashboard/Applications/VideoConference/VideoPlayer/Settings/SettingsModal/SettingsModal.tsx
+++ components/features/Dashboard/Applications/VideoConference/VideoPlayer/Settings/SettingsModal/SettingsModal.tsx
@@ -1,11 +1,134 @@
 import React, { useEffect, useState } from "react";
+import styles from "./SettingsModal.module.css";
+import Select from "react-select";
+import { Button } from "@components/shared/elements/Button";
+import { useDevices, DeviceType } from "@100mslive/react-sdk";
+import { motion } from "framer-motion";
 import { createPortal } from "react-dom";
 
-const DeviceSettings = () => {
-    return <></>
+interface SettingsProps {
+  setSettingsOpened: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-const SettingsModal = () => {
+const DeviceSettings: React.FC<React.PropsWithChildren<SettingsProps>> = ({
+  setSettingsOpened,
+}) => {
+  const { allDevices, selectedDeviceIDs, updateDevice } = useDevices();
+  const { videoInput, audioInput, audioOutput } = allDevices;
+
+  const deviceOptionMapper = (device?: MediaDeviceInfo) => {
+    if (!device) {
+      return;
+    }
+    return {
+      label: device.label,
+      value: { id: device.deviceId, type: device.kind },
+    };
+  };
+
+  const devicesOptionMapper = (devices?: MediaDeviceInfo[]) => {
+    if (!devices) {
+      return;
+    }
+    return devices.map((device) => deviceOptionMapper(device)!);
+  };
+
+  return (
+    <div style={{ overflow: "auto" }} data-testid="SettingsModal">
+      <motion.div
+        className={`${styles.backdrop}`}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 0.2 }}
+        onClick={() => setSettingsOpened(false)}
+      />
+      <motion.div
+        className={`${styles.modal}`}
+        style={{ left: "50%" }}
+        initial={{ opacity: 0, top: 0 }}
+        animate={{ opacity: 1, top: "50%" }}
+        exit={{ opacity: 0, top: "-100%" }}
+      >
+        <h1 className={`${styles["modal-header"]}`}>Settings</h1>
+        <div className={`${styles.devices}`}>
+          <div className={`${styles["device-dropdown-group"]}`}>
+            <h2>Camera</h2>
+            <Select
+              data-label="Audio Input"
+              className={`${styles["device-dropdown"]}`}
+              defaultValue={deviceOptionMapper(
+                videoInput?.find(
+                  (device) => device.deviceId === selectedDeviceIDs.videoInput
+                )
+              )}
+              options={devicesOptionMapper(videoInput)}
+              onChange={(device) =>
+                updateDevice({
+                  deviceType: DeviceType.videoInput,
+                  deviceId: device?.value.id!,
+                })
+              }
+            />
+          </div>
+          <div className={`${styles["device-dropdown-group"]}`}>
+            <h2>Audio Input</h2>
+            <Select
+              data-label="Audio Input"
+              className={`${styles["device-dropdown"]}`}
+              defaultValue={deviceOptionMapper(
+                audioInput?.find(
+                  (device) => device.deviceId === selectedDeviceIDs.audioInput
+                )
+              )}
+              options={devicesOptionMapper(audioInput)}
+              onChange={(device) =>
+                updateDevice({
+                  deviceType: DeviceType.audioInput,
+                  deviceId: device?.value.id!,
+                })
+              }
+            />
+          </div>
+          <div className={`${styles["device-dropdown-group"]}`}>
+            <h2>Audio Output</h2>
+            <Select
+              data-label="Audio Input"
+              className={`${styles["device-dropdown"]}`}
+              defaultValue={deviceOptionMapper(
+                audioOutput?.find(
+                  (device) => device.deviceId === selectedDeviceIDs.audioOutput
+                )
+              )}
+              options={devicesOptionMapper(audioOutput)}
+              onChange={(device) =>
+                updateDevice({
+                  deviceType: DeviceType.audioOutput,
+                  deviceId: device?.value.id!,
+                })
+              }
+            />
+          </div>
+          <div
+            style={{ width: "100%", display: "flex", justifyContent: "end" }}
+          >
+            <Button
+              onClick={() => setSettingsOpened(false)}
+              variant="secondary"
+              style={{ margin: 0, maxWidth: "100px", fontWeight: "bold" }}
+            >
+              close
+            </Button>
+          </div>
+        </div>
+      </motion.div>
+    </div>
+  );
+};
+
+const SettingsModal: React.FC<React.PropsWithChildren<SettingsProps>> = ({
+  setSettingsOpened,
+}) => {
   const [inBrowser, setInBrowser] = useState(false);
 
   useEffect(() => {
@@ -13,7 +136,10 @@ const SettingsModal = () => {
   }, []);
 
   if (inBrowser) {
-    return <div data-testid="SettingsModal"></div>
+    return createPortal(
+      <DeviceSettings setSettingsOpened={setSettingsOpened} />,
+      document.getElementById("video-conference-modal")!
+    );
   }
 
   return null;
