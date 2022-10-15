4e8b0646698fd0aeb415a29582a3c0d61ee2c1d3
rashadaziz <rashad.aziz@ui.ac.id>
2022-10-15 18:26:12 +0700

    [REFACTOR] refactor date util test
    Refactor date util test to respect UTC timezone to fix github actions issue

--- utils/formatters/dateFormatter/dateFormatter.test.ts
+++ utils/formatters/dateFormatter/dateFormatter.test.ts
@@ -8,15 +8,15 @@ describe("Date formatter utility function test suite", () => {
         expect(typeof formattedDate).toBe("string")
     })
     test("testing same day date (AM)", () => {
-        const nowDate = new Date("2022-04-15T12:00:00+07:00");
-        const beforeDate = new Date("2022-04-15T11:00:00+07:00");
+        const nowDate = new Date("2022-04-15T12:00:00");
+        const beforeDate = new Date("2022-04-15T11:00:00");
         const formattedDate = dateFormatter(beforeDate, nowDate);
         
         expect(formattedDate).toBe("11:00 AM");
     })
     test("testing same day date (PM)", () => {
-        const nowDate = new Date("2022-04-15T23:00:00+07:00");
-        const beforeDate = new Date("2022-04-15T22:00:00+07:00");
+        const nowDate = new Date("2022-04-15T23:00:00");
+        const beforeDate = new Date("2022-04-15T22:00:00");
         const formattedDate = dateFormatter(beforeDate, nowDate);
         
         expect(formattedDate).toBe("10:00 PM");
@@ -36,8 +36,8 @@ describe("Date formatter utility function test suite", () => {
         expect(formattedDate).toBe("15/04/21");
     })
     test("testing different day but within 24 hours date", () => {
-        const nowDate = new Date("2022-04-16T00:00:00+07:00");
-        const beforeDate = new Date("2022-04-15T23:00:00+07:00");
+        const nowDate = new Date("2022-04-16T00:00:00");
+        const beforeDate = new Date("2022-04-15T23:00:00");
         const formattedDate = dateFormatter(beforeDate, nowDate);
 
         expect(formattedDate).toBe("Apr 15");
