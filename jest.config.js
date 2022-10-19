02fc8ea2b1371ae7f70918b92682814fbff7b5ce
alexander.caesario <alexandercaesariobangun@gmail.com>
2022-10-19 15:18:06 +0700

    [CHORES] remove mock from coverage
    Remove mocks from coverage of tests, create useRouter mocks

--- jest.config.js
+++ jest.config.js
@@ -15,6 +15,7 @@ module.exports = {
     '!<rootDir>/*.config.js',
     '!<rootDir>/*.config.ts',
     '!<rootDir>/coverage/**',
+    "!**/mocks/**"
   ],
   moduleNameMapper: {
     // Handle CSS imports (with CSS modules)
