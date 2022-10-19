b536fce32886eb62a842180797364506a669d78a
alexander.caesario <alexandercaesariobangun@gmail.com>
2022-10-19 15:49:18 +0700

    [REFACTOR] refactor company details to use form instead of div
    Refactor HTML element from div to form on CompanyDetails and CompanyPassword, remove console.log from Multistep

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,12 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { AuthProvider } from "@context/Authentication";
 import { MotionConfig } from "framer-motion";
+import { ToastContainer } from "react-toastify";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <MotionConfig reducedMotion="user">
+      <ToastContainer containerId="root-toast"/>
       <AuthProvider>
         <Component {...pageProps} />
       </AuthProvider>
