02b3d1ef972652d117d4db71ceac33e790f3fb82
alexander.caesario <alexandercaesariobangun@gmail.com>
2022-10-20 15:17:08 +0700

    [RED] add additional tests for disabled buttons
    Create tests for disabled buttons, testing their class attributes

--- components/shared/elements/Button/Button.test.tsx
+++ components/shared/elements/Button/Button.test.tsx
@@ -16,7 +16,7 @@ describe("Button element test -- primary", () => {
   it("Test if element rendered properly", () => {
     const buttonElement = screen.getByTestId("button");
     expect(buttonElement).toBeInTheDocument();
-    expect(buttonElement).toHaveAttribute("class", "button button--primary")
+    expect(buttonElement).toHaveAttribute("class", "button button--primary ")
   });
 
   it("Test if event fired when clicked", () => {
@@ -31,7 +31,16 @@ describe("Button element test -- secondary", () => {
     render(<Button variant="secondary" onClick={ mockedFunction }>Hello</Button>);
     const buttonElement = screen.getByTestId("button");
     expect(buttonElement).toBeInTheDocument();
-    expect(buttonElement).toHaveAttribute("class", "button button--secondary")
+    expect(buttonElement).toHaveAttribute("class", "button button--secondary ")
+  })
+})
+
+describe("Button element test -- disabled", () => {
+  it("Test if element rendered properly", () => {
+    render(<Button variant="primary" disabled onClick={ mockedFunction }>Hello</Button>);
+    const buttonElement = screen.getByTestId("button");
+    expect(buttonElement).toBeInTheDocument();
+    expect(buttonElement).toHaveAttribute("class", "button button--primary button--disabled")
   })
 })
 
