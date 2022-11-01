b5e403765e1f48ab775dbc162abfb0ceab3be7f2
rashadaziz <rashad.aziz@ui.ac.id>
2022-11-01 18:12:01 +0700

    [REFACTOR] add automatic redirect for assessee that has unauthorized access to the assessment event

--- pages/dashboard/[assessment-event-id].tsx
+++ pages/dashboard/[assessment-event-id].tsx
@@ -1,8 +1,35 @@
 import type { NextPage } from "next";
 import Dashboard from "@components/features/Dashboard";
 import ProtectedRoute from "@components/shared/layouts/ProtectedRoute";
+import useGetRequest from "@hooks/shared/useGetRequest";
+import { useEffect } from "react";
+import styles from "../../styles/AssesseeDashboard.module.css";
+import { toast } from "react-toastify";
+import { Loader } from "@components/shared/elements/Loader";
+import { useRouter } from "next/router";
 
 const AssessmentEvent: NextPage = () => {
+  const router = useRouter();
+  const { data, error } = useGetRequest(
+    `/assessment/assessment-event/verify-participant/?assessment-event-id=${router.query["assessment-event-id"]}`,
+    { requiresToken: true }
+  );
+
+  useEffect(() => {
+    if (error) {
+      toast.error(error.message);
+      router.push("/");
+    }
+  }, [error, router]);
+
+  if (!data) {
+    <ProtectedRoute>
+      <div className={styles["loader-container"]}>
+        <Loader />
+      </div>
+    </ProtectedRoute>;
+  }
+
   return (
     <ProtectedRoute>
       <Dashboard />
