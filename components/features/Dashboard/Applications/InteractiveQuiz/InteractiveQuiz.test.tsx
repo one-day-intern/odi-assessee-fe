48d56c49970bcf5ee29377ab4fd890a0dcc7bc81
rashadaziz <rashad.aziz@ui.ac.id>
2022-11-04 20:46:04 +0700

    [REFACTOR] refactor interactive quiz test
    add DashboardAPI wrapper to component test suite

--- components/features/Dashboard/Applications/InteractiveQuiz/InteractiveQuiz.test.tsx
+++ components/features/Dashboard/Applications/InteractiveQuiz/InteractiveQuiz.test.tsx
@@ -1,10 +1,16 @@
 import React from "react";
-import InteractiveQuiz from "./InteractiveQuiz"
+import InteractiveQuiz from "./InteractiveQuiz";
 import { render } from "@testing-library/react";
+import DashboardAPIProvider from "../../../../../context/Dashboard/DashboardAPIContext";
 
 describe("InteractiveQuiz component test suite", () => {
-    test("render component", () => {
-        const { getByTestId } = render(<InteractiveQuiz/>);
-        getByTestId("InteractiveQuiz")
-    })
-})
+  test("render component", () => {
+    const dummyRef = { current: document.createElement("div") };
+    const { getByTestId } = render(
+      <DashboardAPIProvider parentRef={dummyRef} onPushNotification={() => {}}>
+        <InteractiveQuiz />
+      </DashboardAPIProvider>
+    );
+    getByTestId("InteractiveQuiz");
+  });
+});
