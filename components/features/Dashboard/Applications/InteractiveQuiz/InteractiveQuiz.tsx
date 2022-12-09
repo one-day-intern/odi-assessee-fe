1fac83700488ca8718ed561be99ec4d36bc7e254
rashadaziz <rashad.aziz@ui.ac.id>
2022-12-09 20:38:27 +0700

    [GREEN] finish integrating response test with backend

--- components/features/Dashboard/Applications/InteractiveQuiz/InteractiveQuiz.tsx
+++ components/features/Dashboard/Applications/InteractiveQuiz/InteractiveQuiz.tsx
@@ -6,12 +6,19 @@ import Assignment from "./Assignment";
 import styles from "./InteractiveQuiz.module.css";
 import DashboardEvent from "../../DashboardEvents";
 import { motion } from "framer-motion";
+import Tabs from "./Tabs";
 
 const InteractiveQuiz = () => {
   const router = useRouter();
+  const [currentTab, setCurrentTab] = useState("quiz");
   const [currentActiveAssignment, setCurrentActiveAssignment] =
     useState<AssignmentObject | null>(null);
-  const { data, error, fetchData } = useGetRequest<AssignmentObject[]>(
+  // const [currentActiveQuiz, setCurrentActiveQuiz] =
+  const {
+    data: assignments,
+    error,
+    fetchData: fetchAssignments,
+  } = useGetRequest<AssignmentObject[]>(
     `/assessment/assessment-event/released-assignments/?assessment-event-id=${router.query["assessment-event-id"]}`,
     {
       requiresToken: true,
@@ -19,29 +26,50 @@ const InteractiveQuiz = () => {
   );
 
   useEffect(() => {
-    const refreshAssignments = () => fetchData();
+    const refreshAssignments = () => {
+      fetchAssignments();
+      setCurrentTab("assignment");
+    };
+    const refreshQuizzes = () => {
+      setCurrentTab('quiz');
+    };
     addEventListener(DashboardEvent.REFRESH_ASSIGNMENTS, refreshAssignments);
-    return () =>
+    addEventListener(DashboardEvent.REFRESH_QUIZZES, refreshQuizzes);
+    return () => {
       removeEventListener(
         DashboardEvent.REFRESH_ASSIGNMENTS,
         refreshAssignments
       );
-  }, [fetchData]);
+      removeEventListener(
+        DashboardEvent.REFRESH_QUIZZES,
+        refreshQuizzes
+      );
+    };
+  }, [fetchAssignments]);
 
   if (error) {
     return <div>Something went wrong, please restart the app</div>;
   }
 
-  const showAssignmentList = !currentActiveAssignment && data;
+  const showAssignmentList =
+    !currentActiveAssignment && assignments && currentTab === "assignment";
+  const showQuizList = currentTab === "quiz";
 
   return (
     <div data-testid="InteractiveQuiz" className={`${styles["app-container"]}`}>
       <div id="quiz-root" className={`${styles["window-body"]}`}>
+        {!(currentActiveAssignment) && <div style={{ marginTop: "2rem", marginBottom: '1rem' }}>
+          <Tabs
+            onChange={(tab) => setCurrentTab(tab)}
+            currentTab={currentTab}
+            tabs={["quiz", "assignment"]}
+          />
+        </div>}
         {showAssignmentList && (
           <div className={`${styles["container"]}`}>
             <h1 style={{ marginTop: 0 }}>My Assignments</h1>
             <div className={`${styles["assignment-list"]}`}>
-              {data.map((assignment, i) => (
+              {assignments.map((assignment, i) => (
                 <motion.button
                   whileTap={{ scale: 0.95 }}
                   onClick={() => setCurrentActiveAssignment(assignment)}
@@ -55,7 +83,21 @@ const InteractiveQuiz = () => {
             </div>
           </div>
         )}
-        {!data && (
+        {showQuizList && (
+          <div className={`${styles["container"]}`}>
+            <h1 style={{ marginTop: 0 }}>My Quizzes</h1>
+            <div className={`${styles["assignment-list"]}`}>
+              <motion.button
+                whileTap={{ scale: 0.95 }}
+                className={`${styles["task-item"]}`}
+              >
+                <div className={`${styles.name}`}>test</div>
+                <div className={`${styles.arrow}`}>&rarr;</div>
+              </motion.button>
+            </div>
+          </div>
+        )}
+        {!assignments && (
           <div className={`${styles["loader-container"]}`}>
             <Loader />
           </div>
